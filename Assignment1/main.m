283f10cfa61d8a58bef177fdd317d05d39db9534
will <will@willlam.net>
2015-12-30 09:06:37 -0500

    Initial Commit

---
+++ Assignment1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment1
+//
+//  Created by William Lam on 2015-12-30.
+//  Copyright © 2015 William Lam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
